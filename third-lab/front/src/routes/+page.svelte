f027117f869174fad49f7a3948e545c41e1ff5e6
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-12 09:50:48 +0300

    feat: setup tailwind in the project

--- third-lab/front/src/routes/+page.svelte
+++ third-lab/front/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="p-4 pt-2">Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
