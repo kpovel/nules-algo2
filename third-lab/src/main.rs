2569e1537f62afd2d085fec354a1031ef2b3e126
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-12 08:52:57 +0300

    feat: add other sort methods to testing

--- third-lab/src/main.rs
+++ third-lab/src/main.rs
@@ -1,8 +1,7 @@
 use actix_web::{get, web, App, HttpServer, Responder, Result};
 use serde::{Deserialize, Serialize};
+use sort::{bubble_sort, insertion_sort, merge_sort, selection_sort};
 use std::time::{Duration, Instant};
-
-use sort::merge_sort;
 
 mod sort;
 
@@ -18,12 +17,45 @@ struct InputSort {
     qty: u32,
 }
 
+#[get("/bubble/{qty}")]
+async fn bubble(input: web::Path<InputSort>) -> Result<impl Responder> {
+    let mut vec = (0..input.qty).rev().collect::<Vec<u32>>();
+    let start = Instant::now();
+    bubble_sort(&mut vec);
+    let end = Instant::now();
+
+    let results = SortResults {
+        method: String::from("Bubble sort"),
+        qty: input.qty,
+        sort_time: end - start,
+    };
+
+    Ok(web::Json(results))
+}
+
+#[get("/insertion/{qty}")]
+async fn insertion(input: web::Path<InputSort>) -> Result<impl Responder> {
+    let mut vec = (0..input.qty).rev().collect::<Vec<u32>>();
+    let start = Instant::now();
+    insertion_sort(&mut vec);
+    let end = Instant::now();
+
+    let results = SortResults {
+        method: String::from("Insertion sort"),
+        qty: input.qty,
+        sort_time: end - start,
+    };
+
+    Ok(web::Json(results))
+}
+
 #[get("/merge/{qty}")]
 async fn merge(input: web::Path<InputSort>) -> Result<impl Responder> {
     let mut vec = (0..input.qty).rev().collect::<Vec<u32>>();
     let start = Instant::now();
     merge_sort(&mut vec);
     let end = Instant::now();
+
     let results = SortResults {
         method: String::from("Merge sort"),
         qty: input.qty,
@@ -33,10 +65,34 @@ async fn merge(input: web::Path<InputSort>) -> Result<impl Responder> {
     Ok(web::Json(results))
 }
 
+#[get("/selection/{qty}")]
+async fn selection(input: web::Path<InputSort>) -> Result<impl Responder> {
+    let mut vec = (0..input.qty).rev().collect::<Vec<u32>>();
+    let start = Instant::now();
+    selection_sort(&mut vec);
+    let end = Instant::now();
+
+    let results = SortResults {
+        method: String::from("Selection sort"),
+        qty: input.qty,
+        sort_time: end - start,
+    };
+
+    Ok(web::Json(results))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::scope("/sort").service(merge)))
-        .bind(("127.0.0.1", 6969))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/sort")
+                .service(bubble)
+                .service(insertion)
+                .service(selection)
+                .service(merge),
+        )
+    })
+    .bind(("127.0.0.1", 6969))?
+    .run()
+    .await
 }
