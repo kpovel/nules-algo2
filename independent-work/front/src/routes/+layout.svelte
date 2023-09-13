b8ecb5b23ae0912729cef3f83c6ee2cd2facc2d2
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-13 11:52:15 +0300

    feat: add navigation menu

--- independent-work/front/src/routes/+layout.svelte
+++ independent-work/front/src/routes/+layout.svelte
@@ -1,5 +1,27 @@
-<script>
+<script lang="ts">
   import "../app.css";
+  import { page } from "$app/stores";
+  $: currentPath = $page.url.pathname;
+
+  const navLinks = [
+    { path: "/", title: "Sort results" },
+    { path: "/about-me", title: "About me" }
+  ];
 </script>
 
-<slot />
+<header class="bg-sky-300">
+  <nav class="flex h-16 items-center justify-center gap-5">
+    {#each navLinks as item}
+      <a
+        href={item.path}
+        class="rounded-lg border px-4 py-2"
+        class:bg-sky-400={currentPath === item.path}
+      >
+        {item.title}
+      </a>
+    {/each}
+  </nav>
+</header>
+<main>
+  <slot />
+</main>
