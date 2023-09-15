af9d90d9e19661f958c7710c84311b00e7025667
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-15 14:24:10 +0300

    feat: add about me information

---
+++ independent-work/front/src/routes/about-me/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Me from "$lib/img/PXL_20230701_022636939.jpg";
+</script>
+<div><span class="font-semibold">Виконав:</span> Карась Павло Петрович</div>
+<div><span class="font-semibold">Факультет:</span> Інформаційних технологій</div>
+<div><span class="font-semibold">Спеціальність:</span> Інженерія програмного забезпечення</div>
+<div><span class="font-semibold">Курс:</span> 3</div>
+<div><span class="font-semibold">Група:</span> ІПЗ-21007б</div>
+
+<img src={Me} alt="me" class="object-contain h-96">
